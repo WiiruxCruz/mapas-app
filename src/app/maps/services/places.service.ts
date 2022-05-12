019da1df4e64cd0e3b19d3d04697072f28244adf
WiiruxCruz <hialf.92@gmail.com>
2022-05-12 17:42:09 -0500

    Realizar peticion HTTP para obtener lugares

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PlacesResponse, Feature } from '../interfaces/places';
 
 @Injectable({
 	providedIn: 'root'
@@ -7,11 +9,14 @@ export class PlacesService {
 
 public useLocation?: [number, number];
 
+public isLoadingPLaces: boolean = false;
+public places: Feature[] = [];
+
 	get isUserLocationReady(): boolean {
 		return !!this.useLocation; //Primer navegación es que tenga un valor y el segundo es la negación
 	}
 
-	constructor() {
+	constructor( private http: HttpClient) {
 		this.getUserLocation();
 	}
 
@@ -31,5 +36,17 @@ public useLocation?: [number, number];
 				}
 			);
 		});
+	}
+
+	getPlacesByQuery( query: string = '' ) {
+		this.isLoadingPLaces = true;
+		this.http.get<PlacesResponse>(`https://api.mapbox.com/geocoding/v5/mapbox.places/${ query }.json?proximity=-99.1365151461231%2C19.445519042186064&types=place%2Cpostcode%2Caddress&language=es&access_token=pk.eyJ1Ijoid2lpcnV4Y3J1eiIsImEiOiJjbDBodDhjbTkwN3BmM2Vxd3ZuY21reG85In0.J6npKCfkCbG86unIOUzWaQ`)
+		.subscribe( resp => {
+				console.log( resp.features )
+
+				this.isLoadingPLaces = false;
+				this.places = resp.features;
+			}
+		);
 	}
 }
